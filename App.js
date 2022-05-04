1d470872d58af75ebcbcf47574bfbf85b2d01137
Jacks Bruno Moran Prado <jacks.prado@grupopereira.local>
2022-05-04 09:39:06 -0400

    Projeto de inseção de notas utilizando SQLite

---
+++ App.js
@@ -0,0 +1,42 @@
+import { SafeAreaView, StatusBar, StyleSheet, FlatList } from "react-native"
+import NotaEditor from "./src/componentes/NotaEditor"
+import { useState, useEffect } from "react"
+import { Nota } from './src/componentes/Nota'
+import { buscaNotas, criaTabela } from "./src/componentes/servicos/Notas"
+
+export default function App() {
+  useEffect(() => {
+    criaTabela()
+    mostraNotas()
+  }, [])
+
+  const [notaSelecionada, setNotaSelecionada] = useState({})
+  const [notas, setNotas] = useState([])
+
+  async function mostraNotas(){
+    const todasNotas = await buscaNotas()
+    setNotas(todasNotas)
+    console.log(todasNotas)
+  }
+
+  return (
+    <SafeAreaView style={estilos.container}>
+      <FlatList
+        data={notas}
+        renderItem={nota => <Nota {...nota} setNotaSelecionada={setNotaSelecionada} />}
+        keyExtractor={nota => nota.id}
+      />
+      <NotaEditor mostraNotas={mostraNotas} notaSelecionada={notaSelecionada} setNotaSelecionada={setNotaSelecionada}/>
+      <StatusBar/>
+    </SafeAreaView>
+  )
+}
+
+const estilos = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "stretch",
+		justifyContent: "flex-start",
+	},
+})
+
